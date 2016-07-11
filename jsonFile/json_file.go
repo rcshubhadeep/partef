779fcac9bacee23f1c4658e65a3890d9ce417f95
Shubhadeep <shubhadeeproychowdhury@gmail.com>
2016-07-11 23:19:51 +0200

    Part of basic setup done

---
+++ jsonFile/json_file.go
@@ -0,0 +1,12 @@
+package jsonFile
+
+import (
+	"os"
+)
+
+func FileExists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
---
+++ partef.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"partef/jsonFile"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+
+	var concReq int
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:        "conc, c",
+			Value:       10,
+			Usage:       "Number of concurent requests",
+			Destination: &concReq,
+		},
+	}
+
+	app.Name = "partef"
+	app.Usage = "Load testing of any REST API."
+	app.Version = "0.1"
+	app.UsageText = "partef [global options] <filename.json>"
+
+	app.Action = func(c *cli.Context) error {
+		fileName := "api.json"
+		if c.NArg() > 0 {
+			fileName = c.Args()[0]
+		}
+		if jsonFile.FileExists(fileName) == false {
+			fmt.Println("File does not exists")
+			return nil
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
+}
